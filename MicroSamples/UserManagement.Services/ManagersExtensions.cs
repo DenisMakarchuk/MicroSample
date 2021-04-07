7c2ac86b665bbc212724ec8655448a61f21621be
Denis Makarchuk <hiedmant@gmail.ru>
2021-04-07 20:48:46 +0300

    Empty project

---
+++ MicroSamples/UserManagement.Services/ManagersExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManagement.Data;
+
+namespace UserManagement.Services
+{
+    public static class ManagersExtensions
+    {
+        public static IServiceCollection AddManagersServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDataServices(config);
+            services.AddAutoMapper(typeof(MapperLogicModule));
+
+            return services;
+        }
+    }
+}
